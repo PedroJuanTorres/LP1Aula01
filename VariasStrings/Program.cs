37ebf140a8039c5954932fbee9796d17b5cd1412
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-02-24 12:24:48 +0000

    Add VariasStrings project

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"\nHello World!\n");
+            Console.WriteLine("\u07F7 \u09EA");
+            Console.WriteLine(@"\u07F7 \u09EA");
+            Console.WriteLine("\"This is a double quotes\"");
+            Console.WriteLine(@"""This is a double quotes""");
+        }
+    }
+}
