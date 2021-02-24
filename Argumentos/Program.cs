fd04e9d0d5ebbb520ddf379f564a6945ae936c2e
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-02-24 11:04:09 +0000

    Comment code

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,12 +2,20 @@
 
 namespace Argumentos
 {
+    /// <summary>
+    /// This is a really cool program!
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Program Starts here.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         static void Main(string[] args)
         {
-            for (int i=0; i < args.Length; i++);
+            for (int i = 0; i < args.Length; i++) ;
             {
+                // This will write argument in console
                 Console.WriteLine(args[i]);
             }
         }
--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,11 +2,20 @@
 
 namespace OlaMundo
 {
+    /// <summary>
+    /// This is a program that says Hello and Bye
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The program starts here
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         static void Main(string[] args)
         {
+            //This line prints Hello World on the console
             Console.WriteLine("Hello World!");
+            //This line prints Bye World on the console
             Console.WriteLine("Bye World!");
         }
     }
