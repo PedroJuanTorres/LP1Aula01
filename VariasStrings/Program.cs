38822ac7fd155c9262c792940ed4f46e0184676f
Letícia Oliveira Torres <leticia@MacBook-Air-de-Leticia.local>
2021-02-24 12:48:35 +0000

    Addind Strings to VariasStrings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -7,6 +7,9 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            int x = 123;
+            int y = -123;
+
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Hello World!");
             Console.WriteLine(@"\nHello World!\n");
@@ -14,6 +17,19 @@ namespace VariasStrings
             Console.WriteLine(@"\u07F7 \u09EA");
             Console.WriteLine("\"This is a double quotes\"");
             Console.WriteLine(@"""This is a double quotes""");
+
+            Console.WriteLine("a" + 2);
+            Console.WriteLine("abc" + x);
+
+            string myStr = $"valor de x é {x}";
+            Console.WriteLine(myStr);
+            myStr = $"{x} mais {y} é igual a {x + y}";
+            Console.WriteLine(myStr);
+
+            myStr = string.Format("Y is {1}, X is {0}", x, y);
+            Console.WriteLine(myStr);
+            Console.WriteLine("Y is {1}, X is {0}", x, y);
+
         }
     }
 }
